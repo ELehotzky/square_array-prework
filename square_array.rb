acedef70845c086d51cafba2c6341426ca419093
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-19 22:41:25 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
-def square_array(array)
-  # your code here
-  squared = []
-  array.each do |num| 
-    squared << num ** 2 
-  end
-  squared
-end
+# def square_array(array)
+#   # your code here
+#   squared = []
+#   array.each do |num| 
+#     squared << num ** 2 
+#   end
+#   squared
+# end
